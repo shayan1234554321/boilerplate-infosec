aebe09b26474c9cb2f1f44340fc0ec79036e3278
shayan1234554321 <kshayan091@gmail.com>
2023-05-06 02:37:00 +0500

    remember https

--- myApp.js
+++ myApp.js
@@ -1,24 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const helmet = require('helmet')
+const helmet = require("helmet");
 
-app.use(helmet.hidePoweredBy())
+app.use(helmet.hidePoweredBy());
 
-app.use(helmet.frameguard({action: 'deny'}))
+app.use(helmet.frameguard({ action: "deny" }));
 
 app.use(helmet.xssFilter());
 
-app.use(helmet.noSniff())
+app.use(helmet.noSniff());
 
-app.use(helmet.ieNoOpen())
+app.use(helmet.ieNoOpen());
+
+const ninetyDaysInSeconds = 90 * 24 * 60 * 60;
+
+app.use(helmet.hsts({ maxAge: ninetyDaysInSeconds, force: true }));
 
 module.exports = app;
-const api = require('./server.js');
-app.use(express.static('public'));
-app.disable('strict-transport-security');
-app.use('/_api', api);
+const api = require("./server.js");
+app.use(express.static("public"));
+app.disable("strict-transport-security");
+app.use("/_api", api);
 app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+  response.sendFile(__dirname + "/views/index.html");
 });
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
