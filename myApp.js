06362be8dc37a28a3411b7a8beba97927b3c1b6a
shayan1234554321 <kshayan091@gmail.com>
2023-05-06 01:44:30 +0500

    hide powered by

--- myApp.js
+++ myApp.js
@@ -2,50 +2,7 @@ const express = require('express');
 const app = express();
 const helmet = require('helmet')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.use(helmet.hidePoweredBy())
 
 module.exports = app;
 const api = require('./server.js');
